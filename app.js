ab826e3c337166f57ea81534fb8c8954475ed9a4
hsvikas <hsvikas8595@gmail.com>
2024-12-23 00:31:36 +0530

    chang1

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ app.use(
 // Connect to MongoDB
 mongoose
   .connect('mongodb://127.0.0.1:27017/cafeBookingDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+   
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error(err));
